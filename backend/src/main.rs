0f08768efe95d4239dc5f163ae6fdf461f43db2d
jackkendall <jkendall3096@gmail.com>
2025-04-05 13:57:11 +0100

    Set up rolling file logs

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -11,22 +11,33 @@ use axum::{
 };
 use env::EnvironmentVariables;
 use tower_http::cors::CorsLayer;
-use tracing::{Level, info};
-use tracing_subscriber::FmtSubscriber;
+use tracing_appender::rolling;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 const CURRENT_VERSION: usize = 1;
 
 /// A program that deliberately introduces minor errors into text for proofreading practice
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    let file_appender = rolling::daily("logs", "application.log");
+    let (non_blocking_appender, _guard) = tracing_appender::non_blocking(file_appender);
+
+    // Create the file layer with the rolling log configuration
+    let file_layer = tracing_subscriber::fmt::layer()
+        .with_writer(non_blocking_appender)
+        .with_ansi(false); // Disable ANSI color codes in file output
+
     // Initialize tracing subscriber
-    let subscriber = FmtSubscriber::builder()
-        .with_max_level(Level::INFO)
-        .finish();
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| format!("{}=debug", env!("CARGO_CRATE_NAME")).into()),
+        )
+        .with(tracing_subscriber::fmt::layer().with_writer(std::io::stdout))
+        .with(file_layer)
+        .init();
 
-    tracing::subscriber::set_global_default(subscriber).expect("Failed to set tracing subscriber");
-
-    info!("Starting text-mutator");
+    tracing::info!("Starting text-mutator");
 
     let env = EnvironmentVariables::from_env()?;
 
@@ -44,7 +55,7 @@ async fn main() -> anyhow::Result<()> {
 
     let listener = tokio::net::TcpListener::bind(backend_url).await.unwrap();
 
-    println!("listening on {}", listener.local_addr().unwrap());
+    tracing::info!("listening on {}", listener.local_addr().unwrap());
 
     axum::serve(listener, app).await.unwrap();
 

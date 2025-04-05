9cf85e588e358d011935869b7d712cbd72e7437b
jackkendall <jkendall3096@gmail.com>
2025-04-05 11:03:26 +0100

    Get frontend/backend URLs from environment variables

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -3,13 +3,14 @@ mod models;
 mod mutator;
 mod handler;
 
+use anyhow::bail;
 use axum::{
     Router,
     http::{HeaderValue, Method},
     routing::{get, post},
 };
 use handler::*;
-use std::io::{self};
+use std::borrow::Cow;
 use tower_http::cors::CorsLayer;
 use tracing::{Level, info};
 use tracing_subscriber::FmtSubscriber;
@@ -17,7 +18,7 @@ use tracing_subscriber::FmtSubscriber;
 /// A program that deliberately introduces minor errors into text for proofreading practice
 
 #[tokio::main]
-async fn main() -> io::Result<()> {
+async fn main() -> anyhow::Result<()> {
     // Initialize tracing subscriber
     let subscriber = FmtSubscriber::builder()
         .with_max_level(Level::INFO)
@@ -27,8 +28,10 @@ async fn main() -> io::Result<()> {
 
     info!("Starting text-mutator");
 
+    let env = EnvironmentVariables::from_env()?;
+
     let cors = CorsLayer::new()
-        .allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap())
+        .allow_origin(env.frontend_url.parse::<HeaderValue>().unwrap())
         .allow_methods([Method::POST, Method::GET]);
 
     let app = Router::new()
@@ -36,7 +39,9 @@ async fn main() -> io::Result<()> {
         .route("/api/mutate", post(mutate))
         .layer(cors);
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:8080")
+    let backend_url = env.backend_url.to_string();
+
+    let listener = tokio::net::TcpListener::bind(backend_url)
         .await
         .unwrap();
 
@@ -47,3 +52,25 @@ async fn main() -> io::Result<()> {
     Ok(())
 }
 
+#[derive(Clone, Debug)]
+pub struct EnvironmentVariables {
+    pub frontend_url: Cow<'static, str>,
+    pub backend_url: Cow<'static, str>,
+}
+
+impl EnvironmentVariables {
+    pub fn from_env() -> anyhow::Result<Self> {
+        dotenv::dotenv().ok();
+
+        Ok(Self {
+            frontend_url: match dotenv::var("MUTATOR_FRONTEND_URL") {
+                Ok(url) => url.into(),
+                Err(err) => bail!("missing frontend URL: {err}"),
+            },
+            backend_url: match dotenv::var("MUTATOR_BACKEND_URL") {
+                Ok(url) => url.into(),
+                Err(err) => bail!("missing backend URL: {err}"),
+            },
+        })
+    }
+}

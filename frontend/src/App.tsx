02bebd9fdb8f2362c6b78b301b1e11371f079a0d
jackkendall <jkendall3096@gmail.com>
2025-04-07 13:25:00 +0100

    Organise app into own component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,51 +1,10 @@
-import { useEffect, useState } from 'react'
-import { apiService as api } from './api.tsx'
 import './App.css'
-import { MutationRequest, MutationResponse } from './models.tsx'
+import Mutator from './mutator/Mutator.tsx'
 
 function App() {
-  const [mutation, setMutation] = useState<MutationResponse | undefined>(undefined)
-  const [backendOk, setBackendOk] = useState<boolean>(false)
-
-  const performHealthcheck = async () => {
-    const ok = await api.healthcheck();
-    setBackendOk(ok);
-  };
-
-  const performMutation = async () => {
-    const req: MutationRequest = {
-      text: "Eius saepe enim magnam. Placeat exercitationem quae et omnis sunt dolorum. Molestias cum aut quia consequatur. Omnis explicabo qui est eveniet ipsam ad. Est ut officiis quisquam laudantium dicta. Temporibus autem totam eum ea autem ipsam.",
-      config: {
-        allowHomophones: true,
-        allowPunctuationRemoval: true,
-        allowSwaps: true,
-        mutationRate: 1.0,
-        seed: undefined
-      }
-    };
-
-    const response = await api.mutate(req);
-
-    setMutation(response);
-  };
-
-  useEffect(() => {
-    performHealthcheck();
-  }, []);
-
   return (
     <>
-      <div>
-        <button onClick={() => performMutation()}>
-          Mutate!
-        </button>
-      </div>
-      <p>
-        Response body: {mutation === undefined ? "(not found)" : JSON.stringify(mutation)}
-      </p>
-      <p>
-        Backend: {backendOk.toString()}
-      </p>
+      <Mutator />
     </>
   )
 }

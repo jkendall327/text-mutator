cb011e71e32375fce124c3059571837a1d3651ec
jackkendall <jkendall3096@gmail.com>
2025-04-08 09:45:03 +0100

    Fix error with Tanstack query caching

--- frontend/src/components/mutator/MutationCard.tsx
+++ frontend/src/components/mutator/MutationCard.tsx
@@ -5,25 +5,34 @@ interface MutationCardProps {
     req: MutationRequest;
 }
 
+function performCall(req: MutationRequest) {
+    return async (): Promise<MutationResponse> => {
+        console.log("Fetching mutation for:", req);
+
+        const response = await fetch(`http://0.0.0.0:8080/api/v1/mutate`, {
+            method: "post",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+
+            body: JSON.stringify(req)
+        });
+
+        return await response.json();
+    };
+}
+
 function useMutation(req: MutationRequest) {
+    const isEnabled = !!req.text;
+
     return useQuery({
-        queryKey: ['mutation'], queryFn: async (): Promise<MutationResponse> => {
-            const response = await fetch(`http://0.0.0.0:8080/api/v1/mutate`, {
-                method: "post",
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                },
-
-                body: JSON.stringify(req)
-            })
-
-            return await response.json();
-        }
+        queryKey: ['mutation', req], queryFn: performCall(req), enabled: isEnabled, retry: false
     });
 }
 
 const MutationCard: React.FC<MutationCardProps> = ({ req }) => {
+
     const { data, error, isFetching } = useMutation(req)
 
     return (

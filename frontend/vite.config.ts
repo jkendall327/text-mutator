585427cf361df235b2f4fa4f537b19a9c924ae02
jackkendall <jkendall3096@gmail.com>
2025-04-08 08:45:31 +0100

    Set up Vite proxy for local dev

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost',
+        changeOrigin: true,
+      },
+    },
+  },
+  plugins: [react(), basicSsl()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'main-bundle.js',
+        assetFileNames: 'assets/[name][extname]',
+      },
+    },
+  },})

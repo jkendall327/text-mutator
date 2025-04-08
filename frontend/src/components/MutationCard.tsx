7168b737853f1519f2476d3ba568b94f716ddd65
jackkendall <jkendall3096@gmail.com>
2025-04-08 12:29:53 +0100

    Minor suggestions

--- frontend/src/components/MutationCard.tsx
+++ frontend/src/components/MutationCard.tsx
@@ -39,32 +39,44 @@ const MutationCard: React.FC<MutationCardProps> = ({ req }) => {
     const { status, data, error, isFetching } = useMutation(req)
 
     const [found, setFound] = useState<number>(0);
-    const [done, setDone] = useState<boolean>(false);
+
+    // All mutations have been found.
+    const done: boolean = status === 'success' && !!data && found === data.mutations.length;
 
     function isDisabled(): boolean {
-        if (status === 'pending' || status === 'error') {
+        if (status != 'success' || !data) {
             return true;
         }
 
         return found === data.mutations.length;
     }
 
+    function handleFound(): void {
+        setFound(found + 1)
+    }
+
+    function handleDone(): void {
+        if (status != 'success') {
+            throw new Error("Tried to complete text when the HTTP status was not yet successful.");
+        }
+
+        setFound(data.mutations.length);
+    }
+
     const content =
         <>
-            <p>Mutations:</p> <span>3/{data?.mutations.length}</span>
-            <button disabled={isDisabled()} onClick={() => setFound(found + 1)}>Found one!</button>
-            <button disabled={done} onClick={() => setDone(true)}>Done</button>
-            <p>{data?.mutated_text}</p>
+            <div className="mutations-display">
+                <p>Mutations:</p> <span>{found}/{data?.mutations.length}</span>
+                <button disabled={isDisabled()} onClick={() => handleFound()}>Found one!</button>
+                <button disabled={done} onClick={() => handleDone()}>Done</button>
+                <p>{data?.mutated_text}</p>
+            </div>
         </>
 
     return (
-        <>
-            {
-                isFetching ?
-                    <p>Loading...</p> :
-                    error ? <p>Error! {error.message}</p> : content
-            }
-        </>
+        isFetching ?
+            <p>Loading...</p> :
+            error ? <p>Error! {error.message}</p> : content
     )
 }
 

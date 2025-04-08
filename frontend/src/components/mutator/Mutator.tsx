8c63f0f19c85c57e8a673edb056bca9cde8df40c
jackkendall <jkendall3096@gmail.com>
2025-04-08 09:30:28 +0100

    Get very basic user flow working

--- frontend/src/components/mutator/Mutator.tsx
+++ frontend/src/components/mutator/Mutator.tsx
@@ -2,10 +2,11 @@ import { MutationRequest } from './models.tsx'
 import './Mutator.css'
 import ServerStatus from '../ServerStatus.tsx'
 import MutationCard from './MutationCard.tsx'
+import { useState } from 'react';
 
 export default function Mutator() {
-    const foo: MutationRequest = {
-        text: "Eius saepe enim magnam. Placeat exercitationem quae et omnis sunt dolorum. Molestias cum aut quia consequatur. Omnis explicabo qui est eveniet ipsam ad. Est ut officiis quisquam laudantium dicta. Temporibus autem totam eum ea autem ipsam.",
+    const [req, setReq] = useState<MutationRequest>({
+        text: "",
         config: {
             allowHomophones: true,
             allowPunctuationRemoval: true,
@@ -13,17 +14,36 @@ export default function Mutator() {
             mutationRate: 1.0,
             seed: undefined
         }
-    };
+    });
+
+    const [text, setText] = useState<string>("");
+
+    function handleClick(): void {
+        setReq({
+            text: text,
+            config: {
+                allowHomophones: true,
+                allowPunctuationRemoval: true,
+                allowSwaps: true,
+                mutationRate: 1.0,
+                seed: undefined
+            }
+        })
+    }
 
     return (
         <>
             <div>
-                <button>
+                <label>
+                    Text input: <input name="myInput" onChange={e => setText(e.target.value)} />
+                </label>
+
+                <button onClick={() => handleClick()}>
                     Mutate!
                 </button>
             </div>
             <MutationCard
-                req={foo}
+                req={req}
             />
             <ServerStatus />
         </>

3b6adb241c26fb5ace849fe4458a48197598217f
jackkendall <jkendall3096@gmail.com>
2025-04-08 10:25:44 +0100

    Add proper labels

--- frontend/src/components/MutationOptionsDisplay.tsx
+++ frontend/src/components/MutationOptionsDisplay.tsx
@@ -37,18 +37,23 @@ const MutationOptionsDisplay: FC<MutationOptionsDisplayProps> = ({ onOptionsChan
     return (
         <>
             <label htmlFor="allowHomophones">
+                Allow homophones
                 <input name="allowHomophones" type="checkbox" checked={options.allowHomophones} onChange={e => handleChange(e)} />
             </label>
             <label htmlFor="allowSwaps">
+                Allow swaps
                 <input name="allowSwaps" type="checkbox" checked={options.allowSwaps} onChange={e => handleChange(e)} />
             </label>
             <label htmlFor="allowPunctuationRemoval">
+                Allow punctuation to be removed
                 <input name="allowPunctuationRemoval" type="checkbox" checked={options.allowPunctuationRemoval} onChange={e => handleChange(e)} />
             </label>
             <label htmlFor="mutationRate">
+                Mutation rate (0 - 1.00)
                 <input name="mutationRate" type="number" value={options.mutationRate} onChange={e => handleChange(e)} />
             </label>
             <label htmlFor="seed">
+                Seed (optional)
                 <input name="seed" type="number" value={options.seed} onChange={e => handleChange(e)} />
             </label>
         </>)

1c69bcc2aa582018340fc4e463c6019004961f60
jackkendall <jkendall3096@gmail.com>
2025-04-10 08:26:21 +0100

    Enable strict mode

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,18 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './App.css'
 import MainDisplay from './components/MainDisplay/MainDisplay'
+import React from 'react'
 
 const queryClient = new QueryClient()
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <MainDisplay />
-    </QueryClientProvider>)
+    <React.StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <MainDisplay />
+      </QueryClientProvider>
+    </React.StrictMode>
+  )
 }
 
 export default App
